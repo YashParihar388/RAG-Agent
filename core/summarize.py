1fba76061ec38091af1d833abca1a9c9a39dec58
Yash Singh Parihar <yparihar8085@gmail.com>
2026-09-12 18:01:14 +0530

    summarize transcript

--- core/summarize.py
+++ core/summarize.py
@@ -2,5 +2,73 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_mistralai import ChatMistralAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+import os
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 
-import os 
+
+def get_llm():
+    return ChatMistralAI(model = "mistral-small-latest", mistral_api_key = os.getenv("MISTRAL_API_KEY"),temperature=0.3)
+
+def split_transcript(transcript: str) -> list:
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 3000,
+        chunk_overlap = 200
+    )
+
+    return splitter.split_text(transcript)
+
+ 
+def summarize(transcript: str) -> str:
+    llm = get_llm()
+
+    map_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "Summarize this portion of a meeting transcript concisely."),
+            ("human", "{text}"),
+        ]
+    )
+
+    map_chain = map_prompt | llm | StrOutputParser()
+
+    chunks = split_transcript(transcript)
+
+    chunk_summaries = []
+
+    for chunk in chunks:
+        summary = map_chain.invoke({"text": chunk})
+        chunk_summaries.append(summary)
+
+    combined = "\n\n".join(chunk_summaries)
+
+    combined_prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are an expert meeting summarizer. Combine these partial summaries "
+                "into one final professional meeting summary in bullet points.",
+            ),
+            ("human", "{text}"),
+        ]
+    )
+
+    combined_chain = combined_prompt | llm | StrOutputParser()
+
+    return combined_chain.invoke({"text": combined})
+
+
+
+def generate_title(transcript: str) -> str:
+    llm = get_llm()
+
+    title_prompt = ChatPromptTemplate.from_messages([
+        (
+            "system",
+            "Based on the meeting transcript, generate a short professional meeting title "
+            "(max 8 words). Only return the title, nothing else.",
+        ),
+        ("human", "{text}"),
+    ])
+
+    title_chain = title_prompt | llm | StrOutputParser()
+
+    return title_chain.invoke({"text": transcript[:2000]})
